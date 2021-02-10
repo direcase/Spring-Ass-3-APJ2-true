08c4d80640ddb997e5dbd5ed952c0a52b4155ce5
майа хуху <dariya.bigabulova@gmail.com>
2021-02-10 12:53:12 +0600

    a

--- src/main/java/com/example/SpringAss3APJ2/controller/BankController.java
+++ src/main/java/com/example/SpringAss3APJ2/controller/BankController.java
@@ -129,7 +129,7 @@ public class BankController {
         List<History> histories=bankService.showAll(id);
         Collections.sort(histories,new Sort());
         List<History> hisList = new ArrayList<>();
-        for (int i=0; i<histories.size(); i++) {
+        for (int i=histories.size()-1; i>=0; i--) {
             System.out.println(histories.get(i));
             hisList.add(histories.get(i));
         }
